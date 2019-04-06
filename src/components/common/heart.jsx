b8495b715eb380a16d3b0cdb1081b4d2fafe4908
pwildenhain <pwildenhain@gmail.com>
2019-04-06 00:04:30 -0400

    Use propTypes on Heart

--- src/components/common/heart.jsx
+++ src/components/common/heart.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Heart = (props) => {
-    const {liked, toggleHeart} = props;
-        let heartClasses = "fa fa-heart";
-        if (!liked) heartClasses += "-o"
-    return (<i className={heartClasses} aria-hidden="true" style={{cursor: "pointer"}} onClick={toggleHeart}></i>)
-}
+const Heart = props => {
+  const { liked, toggleHeart } = props;
+  let heartClasses = "fa fa-heart";
+  if (!liked) heartClasses += "-o";
+  return (
+    <i
+      className={heartClasses}
+      aria-hidden="true"
+      style={{ cursor: "pointer" }}
+      onClick={toggleHeart}
+    />
+  );
+};
+
+Heart.propTypes = {
+  liked: PropTypes.bool,
+  toggleHeart: PropTypes.func.isRequired
+};
 
 export default Heart;
- 
-
