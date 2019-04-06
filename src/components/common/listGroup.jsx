f9e542cf65515a3069b084f997de714b717ca3e7
pwildenhain <pwildenhain@gmail.com>
2019-04-06 00:06:15 -0400

    Add ListGroup for filtering

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ListGroup = props => {
+  const {
+    options,
+    keyProperty,
+    textProperty,
+    selectedOption,
+    onSelectOption
+  } = props;
+  return (
+    <ul className="list-group">
+      {options.map(option => (
+        <button
+          className={
+            option[textProperty] === selectedOption[textProperty]
+              ? "list-group-item list-group-item-action active"
+              : "list-group-item list-group-item-action"
+          }
+          onClick={() => onSelectOption(option)}
+          style={{ cursor: "pointer" }}
+          key={option[keyProperty]}
+        >
+          {option[textProperty]}
+        </button>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  keyProperty: "_id",
+  textProperty: "name",
+  selectedOption: {}
+};
+
+ListGroup.propTypes = {
+  options: PropTypes.array.isRequired,
+  keyProperty: PropTypes.string.isRequired,
+  textProperty: PropTypes.string.isRequired,
+  onSelectOption: PropTypes.func.isRequired,
+  selectedOption: PropTypes.object
+};
+
+export default ListGroup;
